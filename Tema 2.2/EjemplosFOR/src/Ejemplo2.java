0ffc1b99eb87ab7cf3b46800ff08019a9f49f402
Luis-Bernabeu-Fuster <luiberfus@alu.edu.gva.es>
2024-10-28 11:20:13 +0100

    Delete Tema 2.2 directory

--- Tema 2.2/EjemplosFOR/src/Ejemplo2.java
+++
@@ -1,7 +0,0 @@
-public class Ejemplo2 {
-    public static void main(String[] args) {
-        for (int i = 1; i <= 4; ++i) {
-            System.out.println("This is a for loop.");
-        }
-    }
-}
